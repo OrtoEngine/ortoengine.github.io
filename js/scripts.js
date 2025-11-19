e1e02dfd2579154c10e3e587dab1ff73d71d5095
OrtoEngine <ortoengine@gmail.com>
2025-11-19 19:44:06 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+<script>
+    const btn = document.getElementById('btnMissao');
+    const box = document.getElementById('textoMissao');
+
+    btn.addEventListener('click', () => {
+        box.style.display = box.style.display === "none" ? "block" : "none";
+    });
+</script>
