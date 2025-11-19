5018866076f8d793eb45320d0063635e860c72cf
OrtoEngine <ortoengine@gmail.com>
2025-11-19 20:03:01 -0300

    Update scripts.js
    Adicionar botão com animação

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-<script>
-    const btn = document.getElementById('btnMissao');
-    const box = document.getElementById('textoMissao');
+const btn = document.getElementById('btnMissao');
+const box = document.getElementById('textoMissao');
 
-    btn.addEventListener('click', () => {
-        box.style.display = box.style.display === "none" ? "block" : "none";
-    });
-</script>
+btn.addEventListener('click', () => {
+    box.style.display = box.style.display === "none" ? "block" : "none";
+});
+
