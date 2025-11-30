3b400388582ce0d1a7363a19df2edf919565b131
OrtoEngine <ortoengine@gmail.com>
2025-11-30 00:41:00 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
-const btn = document.getElementById('btnMissao');
-const box = document.getElementById('textoMissao');
+<script>
+    const btnMissao = document.getElementById('btnMissao');
+    const btnSolucoes = document.getElementById('btnSolucoes');
 
-btn.addEventListener('click', () => {
-    box.style.display = box.style.display === "none" ? "block" : "none";
-});
+    const textoMissao = document.getElementById('textoMissao');
+    const textoSolucoes = document.getElementById('textoSolucoes');
+
+    function esconderTodos() {
+        textoMissao.style.display = 'none';
+        textoSolucoes.style.display = 'none';
+    }
+
+    // Botão Missão
+    btnMissao.addEventListener('click', () => {
+        if (textoMissao.style.display === 'none') {
+            esconderTodos();
+            textoMissao.style.display = 'block';
+        } else {
+            textoMissao.style.display = 'none';
+        }
+    });
+
+    // Botão Soluções
+    btnSolucoes.addEventListener('click', () => {
+        if (textoSolucoes.style.display === 'none') {
+            esconderTodos();
+            textoSolucoes.style.display = 'block';
+        } else {
+            textoSolucoes.style.display = 'none';
+        }
+    });
+</script>
 
